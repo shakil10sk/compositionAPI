f33b9d33112066228b543bb7bbe3f408a55d92cf
Shakil Hussain <shakil550sh@gmail.com>
2022-03-12 08:09:32 +0600

    reuse or mixins

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,12 @@
   <!-- <ComputedReplace /> -->
   <!-- <WatchComposition /> -->
   <!-- <ProvideInject /> -->
-  <OlifeCycleHooks />
-  <ClifeCycleHooks />
+  <!-- <OlifeCycleHooks />
+  <ClifeCycleHooks /> -->
+  <!-- <MountedComposition /> -->
+  <!-- <Person /> -->
+  <ClickCounter />
+  <HoverCounter />
 
 </template>
 
@@ -21,9 +25,12 @@
 // import ComputedReplace from './components/ComputedReplace.vue'
 // import WatchComposition from './components/WatchCompositionAPI.vue'
 // import ProvideInject from './components/ProvideInject.vue'
-import OlifeCycleHooks from './components/O_lifeCycleHooks.vue'
-import ClifeCycleHooks from './components/C_lifeCycleHooks.vue'
-
+// import OlifeCycleHooks from './components/O_lifeCycleHooks.vue'
+// import ClifeCycleHooks from './components/C_lifeCycleHooks.vue'
+// import MountedComposition from './components/MountedComposition.vue'
+// import Person from './components/Person.vue'
+import ClickCounter from './components/ClickCounter.vue'
+import HoverCounter from './components/HoverCounter.vue'
 export default {
   name: 'App',
   components: {
@@ -35,8 +42,12 @@ export default {
     // ComputedReplace,
     // WatchComposition
     // ProvideInject
-    OlifeCycleHooks,
-    ClifeCycleHooks
+    // OlifeCycleHooks,
+    // ClifeCycleHooks
+    // MountedComposition
+    // Person
+    HoverCounter,
+    ClickCounter
   }
 }
 </script>
